37f50d2d0fb692444ebff7e9e1539d564dedad7d
Jarod Smith <lbriones.dev@gmail.com>
2025-05-04 00:23:29 -0400

    Config de Repository

---
+++ src/main/java/com/briones/grades/repository/ExamRepository.java
@@ -0,0 +1,9 @@
+package com.briones.grades.repository;
+
+import com.briones.grades.model.Exam;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ExamRepository extends JpaRepository<Exam,Long> {
+}
---
+++ src/main/java/com/briones/grades/repository/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.briones.grades.repository;
+
+import com.briones.grades.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student,Long> {
+}
