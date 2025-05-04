1960229e63941462a59247680b7a120e68b0a79b
Jarod Smith <lbriones.dev@gmail.com>
2025-05-03 22:45:51 -0400

    Start project

---
+++ src/test/java/com/briones/grades/ApiGradesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briones.grades;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGradesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
